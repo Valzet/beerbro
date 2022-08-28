c6fafef54b0e915505f8fa4f5be30079a3837f3d
Vlad <belousov7vlad@gmail.com>
2022-08-29 01:56:25 +0300

    menu add

--- public_html/js/common.js
+++ public_html/js/common.js
@@ -1,29 +1,46 @@
-$(function() {
-
-	//SVG Fallback
-	if(!Modernizr.svg) {
-		$("img[src*='svg']").attr("src", function() {
-			return $(this).attr("src").replace(".svg", ".png");
-		});
-	};
-
-	//Chrome Smooth Scroll
-	try {
-		$.browserSelector();
-		if($("html").hasClass("chrome")) {
-			$.smoothScroll();
-		}
-	} catch(err) {
-
-	};
-
-	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
-	
-});
-
-$(window).load(function() {
+$(window).load(function () {
 
 	$(".loader_inner").fadeOut();
 	$(".loader").delay(400).fadeOut("slow");
 
+
 });
+
+
+const slides = document.querySelectorAll(".menu__item");
+const button = document.querySelectorAll(".menu__button");
+
+let current = 0;
+let prev = 3;
+let next = 1;
+
+for (let i = 0; i < button.length; i++) {
+	button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
+	
+}
+
+console.log(button)
+
+const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+const gotoNext = () => current < 3 ? gotoNum(current + 1) : gotoNum(0);
+const gotoNum = number => {
+	current = number;
+	prev = current - 1;
+	next = current + 1;
+
+	for (let i = 0; i < slides.length; i++) {
+		slides[i].classList.remove("menu__item_type_active");
+		slides[i].classList.remove("menu__item_type_next");
+		slides[i].classList.remove("menu__item_type_prev");
+	}
+	if (next == 3) {
+		next = 0;
+	}
+	if (prev == -1) {
+		prev = 3;
+	}
+
+	slides[current].classList.add("menu__item_type_active");
+	slides[next].classList.add("menu__item_type_next");
+	slides[prev].classList.add("reviews__item_type_prev");
+}
